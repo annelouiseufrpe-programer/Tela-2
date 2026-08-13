10336f89355aba186d8559987d6f14f96df849ce
annelouiseufrpe-programer <annelouiseufrpe@gmail.com>
2026-08-13 15:34:27 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -1,1 +1,31 @@
 
+<?php
+
+session_start();
+
+if (!isset($_SESSION["usuario"])) {
+    header("Location: index.php");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Painel</title>
+</head>
+
+<body>
+
+    <h1>Bem-vindo!</h1>
+
+    <p>Você está logado no sistema.</p>
+
+    <a href="logout.php">Sair</a>
+
+</body>
+
+</html>
